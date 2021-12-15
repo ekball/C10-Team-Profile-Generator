25d25ecb3cbec8fa7e609f28584b87f24406c9dc
ekball <90882418+ekball@users.noreply.github.com>
2021-12-14 22:59:10 -0500

    created tests for all 4 constructors to create their respective objects (1 test per constructor) and made them pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an Employee object', () => {
-    const employee = new Employee('John');
+    const employee = new Employee('John', 334960, 'email@email.com');
   
     expect(employee.name).toBe('John');
     expect(employee.id).toEqual(expect.any(Number));
--- index.js
+++ index.js
@@ -1,1 +1,78 @@
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+
+// // function to ask the user questions about their team
+// const questions = () => {
+
+//     return inquirer.prompt([
+//     {
+//         type: 'input',
+//         name: 'title',
+//         message: 'What is the title of your project? (Required)',
+//         validate: titleInput => {
+//           if (titleInput) {
+//             return true;
+//           } else {
+//             console.log('Please enter the title of your project!');
+//             return false;
+//           }
+//         }
+//       },
+//       {
+//         type: 'input',
+//         name: 'description',
+//         message: 'Please describe your project. (Required)',
+//         validate: descriptionInput => {
+//           if (descriptionInput) {
+//             return true;
+//           } else {
+//             console.log('Please enter the description of your project!');
+//             return false;
+//           }
+//         }
+//       },
+//       {
+//         type: 'input',
+//         name: 'test',
+//         message: 'Enter examples for running tests (Optional)',
+//       },
+//       {
+//         type: 'input',
+//         name: 'github',
+//         message: 'What is your github username?',
+//         validate: githubInput => {
+//           if (githubInput) {
+//             return true;
+//           } else {
+//             console.log('Please enter your github username!');
+//             return false;
+//           }
+//         }
+//       },
+//       {
+//         type: 'input',
+//         name: 'email',
+//         message: 'What is your email address?',
+//         validate: emailInput => {
+//           if (emailInput) {
+//             return true;
+//           } else {
+//             console.log('Please enter your email address!');
+//             return false;
+//           }
+//         }
+//       },
+//     ])
+// };
+
+// function init() {
+
+//     // ask the questions, then generate the site from answers
+//     questions()
+//         // add .then statements here
+
+//         // if something goes wrong, let user know with error statement
+//         .catch(err => {
+//             console.log(err);
+//         });
+// }
