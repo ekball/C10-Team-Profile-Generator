829a5c4687c22245e47ff84b808f05de4eacdd95
ekball <90882418+ekball@users.noreply.github.com>
2021-12-14 22:09:11 -0500

    added return statements to all files in lib/

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,10 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an Employee object', () => {
-    const employee = new Employee();
+    const employee = new Employee('John');
   
-    expect(employee.name).toBe(expect.any(String));
-    expect(employee.value).toEqual(expect.any(Number));
+    expect(employee.name).toBe('John');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
+
